c57714183519709cb9b69b4f64f2c4ea55e25665
Jared Green <jaredryan1997@hotmail.com>
2024-03-13 13:06:15 -0300

    Added tabs to part B
    I got the tabs added to part B. The page still doesn't load the various sections for each question, however.

--- Parts_B_C/script.js
+++ Parts_B_C/script.js
@@ -3,7 +3,7 @@ window.onload = function () {
 };
 
 function fetchData() {
-  let url = "./QuizTemplates/GeographyQuiz.json";
+  let url = "../Part_A/QuizTemplates/GeographyQuiz.json";
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
@@ -22,29 +22,31 @@ function ShowQuestions(quizData) {
   let tabs = document.querySelector("#tabs");
   let data = quizData;
   quizTitle.innerHTML = data.title;
-  quizBody.innerHTML = "";
-  let cardHTML = "";
+  tabs.innerHTML = "";
   let tabHTML = "";
-
+  let sectionHTML = "";
   // Create the tabs for navigating the quiz
   data.questions.forEach((question, i) => {
-    tabHTML += `<li class = "nav-item>`;
-    tabHTML += `<a class = "nav-link" aria-current = "page" href = "#">Question${i}>`;
+    tabHTML += `<li class = "nav-item">`;
+    tabHTML += `<a class = "nav-link" aria-current = "page" href = "#">Question${i}`;
+    tabHTML += `</li>`;
   });
+  console.log(tabHTML);
+  tabs.innerHTML += tabHTML;
 
-  // data.questions.forEach((question, i) => {
-  //   cardHTML += `<div class="card">`;
-  //   cardHTML += `<h3>${question.questionText}</h3>`;
-  //   cardHTML += `<ul>`;
-  //   data.questions[i].choices.forEach((choice, j) => {
-  //     console.log(choice);
-  //     cardHTML += `<li class="list-group-item">`;
-  //     cardHTML += `<input type = "radio" id = Q${i}Choice${j} name = Question${i} value = ${data.questions[i].answer}>`;
-  //     cardHTML += `<label for = "Questions">${choice}</label></li>`;
-  //   });
-  //   cardHTML += `</ul>`;
-  //   cardHTML += `</div>`;
-  // });
+  data.questions.forEach((question, i) => {
+    //   sectionHTML += `<div class="card">`;
+    //   sectionHTML += `<h3>${question.questionText}</h3>`;
+    //   sectionHTML += `<ul>`;
+    //   data.questions[i].choices.forEach((choice, j) => {
+    //     console.log(choice);
+    //     sectionHTML += `<li class="list-group-item">`;
+    //     sectionHTML += `<input type = "radio" id = Q${i}Choice${j} name = Question${i} value = ${data.questions[i].answer}>`;
+    //     sectionHTML += `<label for = "Questions">${choice}</label></li>`;
+    //   });
+    //   sectionHTML += `</ul>`;
+    //   sectionHTML += `</div>`;
+  });
   // console.log(cardHTML);
   // quizBody.innerHTML = cardHTML;
 }
